b10494cdb4cc5a7252dd53cb54c77657dcca216c
Thiago Cavalcanti Reis <Thiago_Cavalcanti@gap.com>
2019-12-12 16:08:16 -0300

    test

--- src/main/java/com/spring/amqp/configuration/AmqpConfiguration.java
+++ src/main/java/com/spring/amqp/configuration/AmqpConfiguration.java
@@ -1,10 +1,25 @@
 package com.spring.amqp.configuration;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.AsyncRabbitTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,22 +30,70 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class AmqpConfiguration {
 
-    public static final String BOOKING_EVENT_QUEUE = "bookingEventQueue";
+    private static final String REPLY_QUEUE_NAME = "bookingEventQueue-replyTo";
     private final RabbitTemplate rabbitTemplate;
 
-    @PostConstruct
-    public void init() {
-        rabbitTemplate.setConfirmCallback((correlation, ack, reason) -> {
-            log.info("Received " + (ack ? " ack " : " nack ") + "for correlation: " + correlation);
-        });
-        rabbitTemplate.setReturnCallback((message, replyCode, replyText, exchange, routingKey) -> {
-            log.info("Returned: " + message + "\nreplyCode: " + replyCode
-                    + "\nreplyText: " + replyText + "\nexchange/rk: " + exchange + "/" + routingKey);
-        });
+    @Value("${rabbitmq.exchange}")
+    private String rabbitMqExchange;
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(rabbitMqExchange);
     }
 
     @Bean
-    public Queue bookingEventQueue() {
-        return new Queue(BOOKING_EVENT_QUEUE);
+    Queue replyQueue() {
+        return QueueBuilder.durable(REPLY_QUEUE_NAME).build();
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        rabbitTemplate.setExchange(rabbitMqExchange);
+        rabbitTemplate.setBeforePublishPostProcessors(messagePostProcessor());
+        rabbitTemplate.setMessageConverter(messageConverter());
+    }
+
+    @Bean
+    public AsyncRabbitTemplate asyncRabbitTemplate(RabbitTemplate rabbitTemplate, SimpleMessageListenerContainer container) {
+        AsyncRabbitTemplate asyncRabbitTemplate = new AsyncRabbitTemplate(rabbitTemplate, container);
+        asyncRabbitTemplate.setEnableConfirms(true);
+        asyncRabbitTemplate.setMandatory(true);
+        return asyncRabbitTemplate;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer simpleMessageListenerContainer(RabbitTemplate rabbitTemplate) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(rabbitTemplate.getConnectionFactory());
+        container.setQueueNames(REPLY_QUEUE_NAME);
+        //container.afterPropertiesSet();
+        return container;
+    }
+
+    @RabbitListener(queues = "bookingEventQueue")
+    public void consume() {
+        System.out.println("");
+    }
+
+    private MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter(objectMapper());
+    }
+
+    private ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        objectMapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        return objectMapper;
+    }
+
+    private MessagePostProcessor messagePostProcessor() {
+        return message -> {
+            message.getMessageProperties().setDeliveryMode(MessageDeliveryMode.PERSISTENT);
+            return message;
+        };
     }
 }
--- src/main/java/com/spring/amqp/service/impl/MessageServiceImpl.java
+++ src/main/java/com/spring/amqp/service/impl/MessageServiceImpl.java
@@ -3,27 +3,56 @@ package com.spring.amqp.service.impl;
 import com.spring.amqp.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.AmqpMessageReturnedException;
+import org.springframework.amqp.core.AsyncAmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.AsyncRabbitTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class MessageServiceImpl implements MessageService {
 
-    public static final String BOOKING_EXCHANGE = "booking";
     public static final String BOOKING_FLAGGED = "BOOKING.FLAGGED";
-    private final RabbitTemplate rabbitTemplate;
+    private final AsyncRabbitTemplate asyncRabbitTemplate;
+    private final AsyncAmqpTemplate asyncAmqpTemplate;
 
     @Override
     public void send(String message) {
-        rabbitTemplate.convertAndSend(BOOKING_EXCHANGE, BOOKING_FLAGGED, "test");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-           log.info("end");
-        }
+        AsyncRabbitTemplate.RabbitMessageFuture future = asyncRabbitTemplate.sendAndReceive(BOOKING_FLAGGED,
+                new Message("{ \"message\": \"test\"}".getBytes(), new MessageProperties()));
+
+        ListenableFuture<Object> objectListenableFuture = asyncAmqpTemplate.convertSendAndReceive("", "{ \"message\": \"test\"}");
+        objectListenableFuture.get();
+
+        future.completable()
+                .thenApply(Message::getBody)
+                .thenApply(String::new)
+                .thenAccept(result -> {
+                    log.info("Ack: " + result);
+                });
+
+        future.addCallback(result -> {
+            log.info("Ack: " + result);
+        }, throwable -> {
+            if (throwable instanceof AmqpMessageReturnedException) {
+                AmqpMessageReturnedException exception = (AmqpMessageReturnedException) throwable;
+                log.error("Returned: " + exception.getMessage() + "\nreplyCode: " +
+                        exception.getReplyCode() + "\nreplyText: " + exception.getReplyText() + "\nexchange/rk: " +
+                        exception.getExchange() + "/" + exception.getRoutingKey());
+            } else {
+                log.error(throwable.getMessage(), throwable);
+            }
+        });
+
+        future.getConfirm().addCallback(result -> {
+            log.info("Ack: " + result);
+        }, throwable -> {
+            log.error("Error", throwable);
+        });
     }
 }
